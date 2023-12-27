b8447cf709a8e2a04685f112a894136d93073617
soopeach <hsjeon584@gmail.com>
2023-12-27 14:26:07 +0900

    Feat: CombinationRow 컴포저블 구현

---
+++ app/src/main/java/com/soopeach/conviwizard/ui/components/CombinationRow.kt
@@ -0,0 +1,36 @@
+package com.soopeach.conviwizard.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun <T> CombinationRow(
+    rowTitle: String,
+    itemList: List<T>,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ) {
+
+        Text(text = rowTitle)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        LazyRow(
+        ) {
+            items(itemList.size) {
+                SmallCombinationItem("https://picsum.photos/200", "조합 이름")
+            }
+        }
+    }
+}
