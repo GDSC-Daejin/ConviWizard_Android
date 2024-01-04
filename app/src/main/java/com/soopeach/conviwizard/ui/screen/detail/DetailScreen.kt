9c0132646034d05cdd6e9905865195ca334f87a5
soopeach <hsjeon584@gmail.com>
2024-01-04 16:50:56 +0900

    Feat: DetailScreen에 댓글 작성 폼 추가

--- app/src/main/java/com/soopeach/conviwizard/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/soopeach/conviwizard/ui/screen/detail/DetailScreen.kt
@@ -17,6 +17,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -25,6 +29,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.soopeach.conviwizard.R
+import com.soopeach.conviwizard.ui.components.CommentForm
 import com.soopeach.conviwizard.ui.components.Divider
 
 @Composable
@@ -99,6 +104,20 @@ fun DetailScreen(
             }
 
             Spacer(modifier = Modifier.height(12.dp))
+            Divider()
+        }
+
+        item {
+
+            var commentQuery by remember {
+                mutableStateOf("")
+            }
+            CommentForm(commentQuery, {
+                commentQuery = it
+            }) {
+                // TODO: onSendClicked
+            }
+
             Divider()
         }
 
